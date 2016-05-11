d50917a5583be80d886e51c0dc2f1d032d2426e0
Wayne Banks <thestorerun@yahoo.com>
2016-05-10 19:39:12 -0700

    updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
 // Before you begin, take a good look at index.html
 // You don't have to make any changes to that file, but you should be familiar with the structure of the file--especially its id's.
 
@@ -7,7 +12,7 @@
 // 1. To begin, we need a variable to reference the current time when a user loads the page.
 // Create a variable "today" as an instance of the Date object.
 
-// _____
+var today = new Date(); 
 
 // You can read all about the Date object here:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
@@ -17,10 +22,13 @@
 // The Date object gives us a ton of methods to get the data we want.
 // Create variables for the current date (day of the month, 1-31), current month (0-11) and current year (2015).
 // Assign values for each by calling the following methods on the today variable:
-// Date.getDate();
-// Date.getMonth();
-// Date.getFullYear();
-
+ // today.getDate() = 10;
+ // today.getMonth() = 5;
+ // today.getFullYear() = 2016;
+var currentDate = today.getDate();
+var currentMonth = today.getMonth();
+var currentYear = today.getFullYear();
+console.log(today);
 // _____
 // _____
 // _____
@@ -29,9 +37,11 @@
 // Maybe we can get that from the Date object, but let's just make our own array and use the data we already have.
 // Create a month names array using the new keyword and the Array() prototype object.
 // Populate the array with three-letter string abbreviations for each month.
-
+var monthNameArray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 // _____
 
 // 4. Now we have everything we need to create our HTML string. Use jQuery's .html() method.
 
-// $("#todayDate").html(____);
+$("#todayDate").html(monthNameArray[currentMonth] + ' ' + currentDate + ',' + " " + currentYear);
+
+});
